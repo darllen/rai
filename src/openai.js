68ba490dfe7abb8f7d9e5e85056b9adca106dba2
Anunciada <jamilly.d.castro@accenture.com>
2024-04-03 17:02:00 -0300

    usando fs para adicionar e excluir arquivo de audio enviado via request

--- src/openai.js
+++ src/openai.js
@@ -8,21 +8,54 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 async function transcribe(req) {
   try {
-    const audioFile = req.file;
-    if (!audioFile) {
-      throw new Error('Arquivo de áudio não enviado');
-    }
+    
+    const fileData = req.file.buffer; 
+    const filePath = `${process.env.PATH_TO_PROJECT}/rai/src/assets/audio/${req.file.originalname}`;
+
+    await saveFileFromRequest(fileData, filePath);
+
     const transcription = await openai.audio.transcriptions.create({
-      file: audioFile.buffer,
+      file: fs.createReadStream("./src/assets/audio/teste.mp3"),
       model: 'whisper-1',
     });
-    
+
+    await deleteFile(filePath);
+
     return transcription.text;
+
   } catch (error) {
-    //ta entrando aqui
     console.error('Erro ao transcrever áudio:', error);
     throw error;
   }
 }
+
+function saveFileFromRequest(fileData, filePath) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, fileData, err => {
+      if (err) {
+        console.error('Erro ao salvar o arquivo:', err);
+        reject(err);
+      } else {
+        console.log('Arquivo salvo com sucesso.');
+        resolve();
+      }
+    });
+  });
+}
+
+function deleteFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.unlink(filePath, err => {
+      if (err) {
+        console.error('Erro ao excluir o arquivo:', err);
+        reject(err);
+      } else {
+        console.log('Arquivo excluído com sucesso.');
+        resolve();
+      }
+    });
+  });
+}
+
 
 module.exports = { transcribe };
